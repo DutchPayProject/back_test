1d52227114e6a0011c5893680f5c62ff82f06aa6
박현수 <pm6567@naver.com>
2024-12-16 00:39:52 +0900

    initial commit

---
+++ src/main/java/www/project/dutchpay/controller/UpTimePing.java
@@ -0,0 +1,19 @@
+package www.project.dutchpay.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/ping")
+public class UpTimePing {
+        @GetMapping("/check")
+        private String ping() {
+            return "pong";
+        }
+
+        @GetMapping("/jenkins")
+        private String jenkins() {
+            return "pong";
+        }
+    }
